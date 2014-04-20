d53151593762ed4aae6994ee91427e34292041f1
Josh Broton <joshbroton@gmail.com>
2014-04-19 23:06:38 -0500

    SCSS, sassquatch, gruntfile

---
+++ demo/gruntfile.js
@@ -0,0 +1,63 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                files: {
+                    "css/styles.css": "scss/styles.scss"
+                }
+            }
+        },
+        watch: {
+            livereload: {
+                options: { livereload: true },
+                files: ['css/*.css', 'index.html']
+            },
+            sass: {
+                files: 'scss/*.scss',
+                tasks: ['sass']
+            }
+        },
+        imagemin: {
+            png: {
+                options: {
+                    optimizationLevel: 7
+                },
+                files: [
+                    {
+                        // Set to true to enable the following options…
+                        expand: true,
+                        // cwd is 'current working directory'
+                        cwd: 'img/',
+                        src: ['**/*.png'],
+                        // Could also match cwd line above. i.e. project-directory/img/
+                        dest: 'img/compressed/',
+                        ext: '.png'
+                    }
+                ]
+            }
+        },
+        uglify: {
+            my_target: {
+                files: {
+                    'js/app.min.js': 'js/app.js'
+                }
+            }
+        },
+        cssmin: {
+            combine: {
+                files: {
+                    'css/styles.min.css': 'css/styles.css'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    grunt.registerTask('default', ['sass']);
+};
